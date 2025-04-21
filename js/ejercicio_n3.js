cd870b4dd6bd134f637a42d1e3f1bacf3c2489c5
ivangutierrez996 <kiev_96@hotmail.com>
2025-04-21 19:20:15 -0300

    ejercicio 3- correcto

--- js/ejercicio_n3.js
+++ js/ejercicio_n3.js
@@ -1,5 +1,5 @@
-let numero1 = 10;
-let numero2 = 5;
-let suma = numero1 + numero2;
+// Ejercicio 3 - Área de un círculo con radio 7.5
+let radio = 7.5;
+let area = Math.PI * Math.pow(radio, 2);
 
-console.log("La suma es:", suma);
+console.log("El area del círculo con radio 7.5 es:", area);
