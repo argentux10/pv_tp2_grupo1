cc0fe3439587b090273d727be8557bdd02c379da
Jesica Salas <jessicasalas.as@gmail.com>
2025-04-26 00:12:40 -0300

    Agrego ejercicios

---
+++ js/ejercicio_7.js
@@ -0,0 +1,17 @@
+//CDeclarar un array denominado nombres con al menos 6 nombres.
+// Obtener el nombre más largo del array nombres. Las cadenas de textos
+// (String) son listas de caracteres y las listas tienen un largo (length) que indica su tamaño.
+
+let nombres =["maria","marianela"," marcos","juan","pablo","ana","carlos","jose"];
+let suma=0 ;
+let numero =0;
+let nombreMasLargo;
+for(let i=0;i<edades.length;i++){
+    if (nombres.length > numero){
+        numero = nombres.length;
+        nombreMasLargo = nombres[i];
+    }
+
+}
+
+console.log("El nombre con mayor caracteres es: " + nombreMasLargo);
---
+++ js/main_11.js
@@ -0,0 +1,4 @@
+import {calcularKilometros} from "./ejercicio_11.js";
+document.getElementById('btnCalcular').addEventListener('click',calcularKilometros);
+
+
