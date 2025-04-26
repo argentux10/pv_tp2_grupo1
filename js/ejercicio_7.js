c9646ef995446a0501354511f5c7d9fdf8be73e9
Jesica Salas <jessicasalas.as@gmail.com>
2025-04-26 02:26:35 -0300

    correcion de codigo

--- js/ejercicio_7.js
+++ js/ejercicio_7.js
@@ -1,15 +1,20 @@
-//CDeclarar un array denominado nombres con al menos 6 nombres.
+//Declarar un array denominado nombres con al menos 6 nombres.
 // Obtener el nombre más largo del array nombres. Las cadenas de textos
 // (String) son listas de caracteres y las listas tienen un largo (length) que indica su tamaño.
 
-let nombres =["maria","marianela"," marcos","juan","pablo","ana","carlos","jose"];
+let nombres =["maria"," marcos","juan","pablo","ana","carlos","josefina","alejandra"];
+
 let suma=0 ;
 let numero =0;
+
 let nombreMasLargo;
-for(let i=0;i<edades.length;i++){
-    if (nombres.length > numero){
-        numero = nombres.length;
+for(let i=0; i< nombres.length ;i++){
+
+    if (nombres[i].length > numero){
+
+        numero = nombres[i].length;
         nombreMasLargo = nombres[i];
+
     }
 
 }
