94323e3721a5fea76d58f1f045612a3bb6f5d4cf
argentux10 <ezejaljal2001@hotmail.com>
2025-04-28 15:42:27 -0300

    Creacion de la rama ejercicio 10. Resolucion del ejercicio 10.

---
+++ js/ejercicio_10.js
@@ -0,0 +1,10 @@
+// Solicita al usuario que ingrese tres números
+let num1 = parseInt(prompt("Ingrese el primer número entero:"));
+let num2 = parseInt(prompt("Ingrese el segundo número entero:"));
+let num3 = parseInt(prompt("Ingrese el tercer número entero:"));
+
+// Calcula el promedio
+let promedio = (num1 + num2 + num3) / 3;
+
+// Muestra el resultado
+console.log("El promedio de los tres números es: " + promedio);
