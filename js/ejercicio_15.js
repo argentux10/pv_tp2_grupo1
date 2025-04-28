a566aed0cd86ed2f3a3182141c31657a1f2613dd
argentux10 <ezejaljal2001@hotmail.com>
2025-04-28 15:48:55 -0300

    Creacion de la rama ejercicio 15. Resolucion del ejercicio 15.

---
+++ js/ejercicio_15.js
@@ -0,0 +1,12 @@
+function retornarMes(numero) {
+    const meses = [
+        "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+        "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+    ];
+
+    if (numero >= 1 && numero <= 12) {
+        return meses[numero - 1];
+    } else {
+        return "Ese número no corresponde a un mes del año";
+    }
+}
