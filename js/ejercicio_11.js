c38f9354f9d39d45715ba8ee1ae14e8d64dd39d5
Jesica Salas <jessicasalas.as@gmail.com>
2025-05-09 14:38:42 -0300

    Refactorizacion

--- js/ejercicio_11.js
+++
@@ -1,8 +0,0 @@
-export const calcularKilometros =() =>{
-    const kilometros = parseFloat(document.getElementById('txtkilometros').value);
-    const litros = parseFloat(document.getElementById('txtlitros').value);
-    const consumo = litros / kilometros;
-
-    document.getElementById('resultado').innerText = `El consumo de combustible por kilometros: ${consumo.toFixed(2)}`;
-};
-
