1acaee5dab20546c94e67afb580b3ab5d5479255
Jesica Salas <jessicasalas.as@gmail.com>
2025-05-10 00:10:32 -0300

    Subo ejercicio 19

---
+++ js/funciones_n19.js
@@ -0,0 +1,7 @@
+
+export function mostrarDatos(Nombre, Apellido, Libreta) {
+    alert(`Los datos ingresados son:
+    Nombre: ${Nombre} 
+    Apellido: ${Apellido}
+    Libreta Universitaria: ${Libreta}`);
+}
